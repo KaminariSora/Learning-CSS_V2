f7e3575dabe4cc046012caa1eae4460c73ec6cb5
Your NameKaminariSora <nonthacha.h@gmail.com>
2023-10-22 21:59:02 +0700

    Im dead

--- ChickChatDressing/face.js
+++ ChickChatDressing/face.js
@@ -1,6 +1,11 @@
 save = document.querySelector('.save');
+shop = document.querySelector('.shop');
 
 save.addEventListener('click', () => {
     alert('save');
     window.location.href = 'Dressing.html';
 });
+
+shop.addEventListener('click', () => {
+    window.location.href = '../ChickShop/chickShop.html';
+})
