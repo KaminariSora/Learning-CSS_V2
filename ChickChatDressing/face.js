7440055bd27b734c6bf7ae286f135bf0488f07a5
Your NameKaminariSora <nonthacha.h@gmail.com>
2023-10-27 15:45:55 +0700

    update image to canvas

--- ChickChatDressing/face.js
+++ ChickChatDressing/face.js
@@ -1,3 +1,22 @@
+window.onload = function() {
+    var myImage = document.getElementById("myImg");
+    var charScreen = document.getElementById("charScreen");
+    var charCtx = charScreen.getContext("2d");
+
+    // Make sure the image has loaded before drawing it
+    myImage.onload = function() {
+        charCtx.clearRect(0, 0, charScreen.width, charScreen.height);
+        charCtx.drawImage(myImage, 0, 0, charScreen.width, charScreen.height);
+    };
+
+    // If the image is already loaded, draw it immediately
+    if (myImage.complete) {
+        charCtx.clearRect(0, 0, charScreen.width, charScreen.height);
+        charCtx.drawImage(myImage, 0, 0, charScreen.width, charScreen.height);
+    }
+};
+
+
 save = document.querySelector('.save');
 shop = document.querySelector('.shop');
 
